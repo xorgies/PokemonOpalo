91b65594c4a9f68ec02171f1dc31b8ac401252c3
xorgies <xorgies@gmail.com>
2023-12-24 01:29:26 +0100

    cambiada vista pokemon para poner habilidades y movimientos en español y mas datos

--- WEB2/static/js/main.js
+++ WEB2/static/js/main.js
@@ -13,4 +13,8 @@ $(document).ready(function () {
       ]
     }*/
     );
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });
