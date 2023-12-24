3447b3e25d095faefca16c1febc43664496fe6dd
xorgies <xorgies@gmail.com>
2023-12-24 14:15:08 +0100

    añadido habilidades a la web

--- WEB2/index.py
+++ WEB2/index.py
@@ -18,7 +18,7 @@ def calculadora():
 @app.route('/pokemonLista')
 def lista_pokemon():
     dictPokemon= query_db('select id,name from Pokemon')
-    dictPokemon= cambiarFormatoId(dictPokemon)
+    dictPokemon= cambiarFormatoId(dictPokemon,"id")
     dictTipos= aplanarTipos(query_db('select * from pokemon_tipos_view'))
     return render_template('pokemon_lista.html', pokemon=dictPokemon, tipos=dictTipos)
 
@@ -52,7 +52,7 @@ def egg_group(egg):
     egg_group = '%' + egg + '%'
     # TODO: calcular generos y guardarlo en dos nuevas variables (male,female)
     lista_pokemon = query_db('select id,name from Pokemon where compatibility like ?', [egg_group])
-    lista_pokemon= cambiarFormatoId(lista_pokemon)
+    lista_pokemon= cambiarFormatoId(lista_pokemon,"id")
     dictTipos= aplanarTipos(query_db('select * from pokemon_tipos_view'))
     return (render_template('lista_filtrada.html', lista_pokemon=lista_pokemon, titulo=egg, tipos=dictTipos))
 
@@ -68,7 +68,7 @@ def tipos(tipo):
     lista_tipos_str += ')'
     lista_pokemon = query_db('select id, name from Pokemon where id in '+lista_tipos_str)
     tipo = '<img class="img-tipo-filtrado img-center" src="/static/img/tipos/'+tipo+'_xl.png">'
-    lista_pokemon= cambiarFormatoId(lista_pokemon)
+    lista_pokemon= cambiarFormatoId(lista_pokemon,"id")
     dictTipos= aplanarTipos(query_db('select * from pokemon_tipos_view'))
     return render_template('lista_filtrada.html', lista_pokemon=lista_pokemon, titulo=tipo, tipos=dictTipos)
 
@@ -76,6 +76,15 @@ def tipos(tipo):
 def lista_habilidades():
     dictHabilidades= query_db('select * from Habilidades')
     return render_template('habilidades_lista.html', habilidades=dictHabilidades)
+
+@app.route('/habilidad/<int:habilidad_id>')
+def habilidad(habilidad_id):
+    dictHabilidad= query_db('select * from Habilidades where id=?',[habilidad_id],one=True)
+    dictPokemonsHabilidad= query_db('select * from habilidad_pokemons_view where id=?',[habilidad_id])
+    dictPokemonsHabilidad= cambiarFormatoId(dictPokemonsHabilidad,"pokemon_id")
+    dictTipos= aplanarTipos(query_db('select * from pokemon_tipos_view'))
+
+    return render_template('habilidad.html', habilidad=dictHabilidad, pokemons=dictPokemonsHabilidad,tipos=dictTipos)
 
 @app.route('/movimientosLista')
 def lista_movimientos():
@@ -166,13 +175,14 @@ def calcularGeneros(genero):
 
     return [male,female]
 
-def cambiarFormatoId(dictPokemon):
+def cambiarFormatoId(dictPokemon,nombre_id):
     dictPokemonActualizado = []
     for pokemon in dictPokemon:
-        pokemon["id"] = str(pokemon["id"]).zfill(3)
+        pokemon[nombre_id] = str(pokemon[nombre_id]).zfill(3)
         dictPokemonActualizado.append(pokemon)
 
     return dictPokemonActualizado
+
 
 def aplanarTipos(tipos):
     tiposPokemon = {}
